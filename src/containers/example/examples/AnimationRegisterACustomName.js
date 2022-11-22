a805f6f88d4f5c53e91a2c1dca86ae58fd4c955a
Masood Abdolian <m.abdolian@gmail.com>
2022-11-22 22:15:55 +0330

    synced examples

--- src/containers/example/examples/AnimationRegisterACustomName.js
+++ src/containers/example/examples/AnimationRegisterACustomName.js
@@ -11,8 +11,8 @@ setConfig({
       asset: {
         name: {
           'custom-pulse': [
-            { offset: 0, opacity: '0.6', transform: 'scale(1.0)' },
-            { offset: 1, opacity: '0.0', transform: 'scale(1.5)' }
+            { offset: 0, opacity: '0.6', transform: 'scale(1.00)' },
+            { offset: 1, opacity: '0.0', transform: 'scale(1.60)' }
           ]
         }
       }
@@ -23,7 +23,7 @@ setConfig({
 const AnimationRegisterACustomName = () => {
   return (
     <div className="root">
-      <Avatar shape="circle" size="xl">
+      <Avatar shape="circle">
         <Animation
           delay="0000"
           duration="2000"
@@ -48,7 +48,7 @@ const AnimationRegisterACustomNameExample = () => {
   return (
     <div className="ex-animation-register-a-custom-name">
       <AnimationRegisterACustomName />
-      <style>{`.ex-animation-register-a-custom-name .root {  text-align: center;  padding: 1.5rem;}.ex-animation-register-a-custom-name plus-animation {  border: solid 5px black;  display: inline-block;  position: absolute;  inset: 0;  border-radius: 50%;  z-index: -1;}`}</style>
+      <style>{`.ex-animation-register-a-custom-name .root {  text-align: center;  padding: 1.5rem;}.ex-animation-register-a-custom-name plus-animation {  border: solid 3px black;  display: inline-block;  position: absolute;  inset: 0;  border-radius: 50%;  z-index: -1;}`}</style>
     </div>
   )
 };
