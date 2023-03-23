9f6534bc8d5934b91a2240bd855377c9cc81384a
Masood Abdolian <m.abdolian@gmail.com>
2023-03-23 15:30:58 +0430

    added more examples

---
+++ src/examples/AvatarTooltip.js
@@ -0,0 +1,23 @@
+/**************************************************
+ * THIS FILE IS AUTO-GENERATED, DO NOT EDIT MANUALY
+ **************************************************/
+
+import { Avatar, Center, Tooltip } from '@htmlplus/react';
+const AvatarTooltip = () => {
+  return (
+    <Center>
+      <Avatar>AB</Avatar>
+      <Tooltip>Tooltip</Tooltip>
+    </Center>
+  );
+};
+
+const AvatarTooltipExample = () => {
+  return (
+    <div className="ex-avatar-tooltip">
+      <AvatarTooltip />
+    </div>
+  )
+};
+
+export default AvatarTooltipExample;
