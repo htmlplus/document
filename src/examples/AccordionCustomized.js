42a8dbfa4d5e3918a98efc925f890f70a1ce6a85
Masood Abdolian <m.abdolian@gmail.com>
2023-04-17 18:37:10 +0330

    updated

--- src/examples/AccordionCustomized.js
+++ src/examples/AccordionCustomized.js
@@ -23,7 +23,7 @@ const AccordionCustomizedExample = () => {
   return (
     <div className="ex-accordion-customized">
       <AccordionCustomized />
-      <style>{`.ex-accordion-customized plus-accordion {  border: 0;  border-radius: 0;}.ex-accordion-customized plus-accordion:not(:last-of-type) {  border-bottom: solid 2px lightgray;}.ex-accordion-customized plus-accordion::part(header),.ex-accordion-customized plus-accordion::part(body),.ex-accordion-customized plus-accordion::part(svg) {  transition: all 0.5s ease-out;}.ex-accordion-customized plus-accordion::part(header) {  column-gap: 0.5rem;  flex-direction: row-reverse;  padding-left: 0;}.ex-accordion-customized plus-accordion[state^='expand']::part(header) {  background-color: lightgray;  font-weight: bold;}.ex-accordion-customized plus-accordion[state^='expand']::part(svg) {  fill: gray;  transform: rotate(90deg);}.ex-accordion-customized plus-accordion[state^='collap']::part(svg) {  fill: gray;  transform: rotate(0deg);}`}</style>
+      <style>{`.ex-accordion-customized plus-accordion {  border: 0;  border-radius: 0;}.ex-accordion-customized plus-accordion:not(:last-of-type) {  border-bottom: solid 2px lightgray;}.ex-accordion-customized plus-accordion::part(body),.ex-accordion-customized plus-accordion::part(svg) {  transition: all 0.5s ease-out;}.ex-accordion-customized plus-accordion::part(header) {  column-gap: 0.5rem;  flex-direction: row-reverse;  padding-left: 0;}.ex-accordion-customized plus-accordion[state^='expand']::part(header) {  font-weight: bold;}.ex-accordion-customized plus-accordion[state^='expand']::part(svg) {  fill: gray;  transform: rotate(90deg);}.ex-accordion-customized plus-accordion[state^='collap']::part(svg) {  fill: gray;  transform: rotate(0deg);}`}</style>
     </div>
   )
 };
