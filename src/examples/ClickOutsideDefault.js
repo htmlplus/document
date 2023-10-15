f26a165fd4d4be9e0a22aee92521f9e59b836665
Masood Abdolian <m.abdolian@gmail.com>
2023-10-15 18:33:19 +0330

    updated examples

--- src/examples/ClickOutsideDefault.js
+++ src/examples/ClickOutsideDefault.js
@@ -2,39 +2,42 @@
  * THIS FILE IS AUTO-GENERATED, DO NOT EDIT MANUALY
  **************************************************/
 
-import { useState } from 'react';
-import { Card, Center, ClickOutside } from '@htmlplus/react';
-const ClickOutsideDefault = () => {
+import { useRef, useState } from 'react';
+
+import { Card, ClickOutside } from '@htmlplus/react';
+
+function App() {
+  const outsideRef = useRef();
+  const insideRef = useRef();
   const [inside, setInside] = useState(0);
   const [outside, setOutside] = useState(0);
-  const onClick = () => {
-    setInside(inside + 1);
-  };
-  const onClickOutside = () => {
-    setOutside(outside + 1);
-  };
+  function onClick() {
+    const count = inside + 1;
+    setInside(count);
+    insideRef.current.innerHTML = `${count}`;
+  }
+  function onClickOutside() {
+    const count = outside + 1;
+    setOutside(count);
+    outsideRef.current.innerHTML = `${count}`;
+  }
   return (
-    <Center>
-      <ClickOutside
-        onClick={() => onClick()}
-        onClickOutside={() => onClickOutside()}
-      >
-        <Card elevation="10">
-          <Card.Body>
-            <b>{inside}</b> time(s) inside clicked
-            <br />
-            <b>{outside}</b> time(s) outside clicked
-          </Card.Body>
-        </Card>
-      </ClickOutside>
-    </Center>
+    <ClickOutside onClick={onClick} onClickOutside={onClickOutside}>
+      <Card elevation={10}>
+        <Card.Body>
+          <b ref={insideRef}>0</b> time(s) inside clicked
+          <br />
+          <b ref={outsideRef}>0</b> time(s) outside clicked
+        </Card.Body>
+      </Card>
+    </ClickOutside>
   );
-};
+}
 
 const ClickOutsideDefaultExample = () => {
   return (
     <div className="ex-click-outside-default">
-      <ClickOutsideDefault />
+      <App />
     </div>
   )
 };
