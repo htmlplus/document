a28db7dc4556b96084570e183cc64a5b111f9ccd
Masood Abdolian <m.abdolian@gmail.com>
2024-10-31 16:28:31 +0330

    fix: Resolve the animation preview issue on the assets page

--- src/app/(internal)/element/animation/names/categories.tsx
+++ src/app/(internal)/element/animation/names/categories.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import { Fragment, useEffect, useState } from 'react';
+import assets from '@htmlplus/ui/animation/assets.json';
 import '@htmlplus/ui/animation/names/all.js';
 
 import { TocItem } from '@/containers';
+import { capitalCase } from 'change-case';
 
-import { Category, getCategories } from './getCategories';
+export type Category = {
+  key: string;
+  title: string;
+  items: Array<Omit<Category, 'items'>>;
+}
 
 export function Categories() {
   const [categories, setCategories] = useState<Category[]>([]);
@@ -13,7 +19,36 @@ export function Categories() {
   const [playing, setPlaying] = useState('');
   
   useEffect(() => {
-    getCategories().then(setCategories);
+    const categories: Category[] = [];
+  
+    for (const file of assets) {
+      if (!file.startsWith('names') || file.endsWith('all.js') || file.endsWith('register.js')) continue;
+
+      const [directoryName, fileName] = file
+        .split('names/')
+        .at(1)!
+        .split('.')
+        .slice(0, -1)
+        .join('.')
+        .split('/')
+        .filter((section) => !!section);
+  
+      if (!categories.some((category) => category.key == directoryName))
+        categories.push({
+          key: directoryName,
+          title: capitalCase(directoryName),
+          items: []
+        });
+  
+      const category = categories.find((category) => category.key == directoryName);
+  
+      category?.items.push({
+        key: fileName,
+        title: capitalCase(fileName)
+      });
+    }
+
+    setCategories(categories);
   }, []);
 
   return categories.map((category) => (
